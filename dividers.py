424d925810459d77f387a1b28a034bedb7347c59
Yvette <chiggery@gmail.com>
2016-11-11 20:04:02 +0000

    Update dividers.py

--- dividers.py
+++ dividers.py
@@ -56,15 +56,18 @@ def d(ZeroForVoltageOneForCurrent):
 
 #for working out how close your guess is to the answer for those e12 and e24 qs
 def d2(N, T, ZeroforVoltsOneforAmps):
+    print(str(N/T))
+    print("for amps it's R1/(R1+R2), for volts it's R2/(R1+R2)")
     R1 = float(input("R1 = "))
     R2 = float(input("R2 = "))
     if ZeroforVoltsOneforAmps == 1:
         Req = 1/((1/R1)+(1/R2))
         print(str(Req/R2))
+        print("R2 is in line with the ammeter")
     elif ZeroforVoltsOneforAmps == 0:
         Req = R1 + R2
         print(str(R2/Req))
-    print(str(N/T))
+        print("R2 being measured by the voltmeter")
 
 #work out the resistance if you know the current across it
 def d3(ZeroinlinewithUnknownOneinlinewithKnown):
@@ -79,6 +82,25 @@ def d3(ZeroinlinewithUnknownOneinlinewithKnown):
         R = "what the fuck mang"
     print(str(R))
 
+#work out the resistance if you know the voltage across it
+def d4(ZeroinlinewithUnknownOneinlinewithKnown):
+    Vt = float(input("Vt = "))
+    Vn = float(input("Vn = "))
+    r = float(input("r = "))
+    if ZeroinlinewithUnknownOneinlinewithKnown == 0:
+        R = (r*Vn)/(Vt - Vn)
+    elif ZeroinlinewithUnknownOneinlinewithKnown == 1:
+        R = (r*(Vt - Vn))/Vn
+    else:
+        R = "what ? ??"
+    print(str(R))
+
 #work out the resistance if you know the voltage
-def d4():
-    
+#see sheet for what's what on this one, it's a bit fucked
+def d5():
+    v1 = float(input("v1 = "))
+    v2 = float(input("v2 = "))
+    r1 = float(input("r1 = "))
+    r2 = float(input("r2 = "))
+    req = r1 + r2
+    print(((r2*v1)/req)-((r1*v2)/req))
